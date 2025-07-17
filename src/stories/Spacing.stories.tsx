4ebecda104419c026746288c16b1f2b745bb3a94
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-17 12:49:41 +0200

    add new style to spacing

--- src/stories/Spacing.stories.tsx
+++ src/stories/Spacing.stories.tsx
@@ -34,7 +34,7 @@ const Style: React.FC = () => {
                 border: 1px solid #ccc;
                 display: grid;
                 max-width: calc(550rem/16);
-                grid-template-columns: 1fr 1fr;
+                grid-template-columns: 1fr 1fr 1fr;
                 border-bottom: none;
                 border-radius: 0.5rem;
                 }
@@ -49,8 +49,19 @@ const Style: React.FC = () => {
                 dd {
                 padding: var(--spacing-sm) var(--spacing-md);
                 border-bottom: 1px solid #ccc;
-                border-right: 1px solid #ccc;
                 font-family: monospace;
+                display: grid;
+                gap: 1ch;
+                grid-template-columns: subgrid;
+                grid-column: 2/4;
+                align-items: center;
+                }
+                
+                .info {
+                background: magenta;
+                height: 1ch;
+                width: var(--story-spacing);
+                display: block;
                 }`
             }
         </style>
@@ -59,15 +70,17 @@ const Style: React.FC = () => {
 
 export const Default: Story = {
     render: () => (
-
         <>
             <Style />
             <dl>
                 {['zero', 'xs', 'sm', 'md', 'lg', 'xl'].map((key) => (
                     <React.Fragment key={key}>
                         <dt>{key}</dt>
-                        <dd>
-                            <SpaceCalc value={`--spacing-${key}`} />
+                        <dd style={{ ['--story-spacing' as any]: `var(--spacing-${key})` }}>
+                            <span>
+                                <SpaceCalc value={`--spacing-${key}`} />
+                            </span>
+                            <span className="info" />
                         </dd>
                     </React.Fragment>
                 ))}
