8d367717cf96ac6ad15e059add8121053c5b6ee9
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-17 12:07:34 +0200

    add spacing style

--- src/stories/Spacing.stories.tsx
+++ src/stories/Spacing.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
 
 const meta: Meta = {
     title: "Atoms/Spacing",
@@ -12,7 +13,65 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const SpaceCalc = ({ value }: { value: string }) => {
+
+    const spacing = React.useMemo(() => {
+
+        const bodyStyle = window.getComputedStyle(document.body);
+        return bodyStyle.getPropertyValue(value);
+    }, [value])
+
+    return <span>{spacing}</span>;
+}
+
+const Style: React.FC = () => {
+
+    return (
+        <style>
+            {
+                `dl {
+                font-size: 1rem;
+                border: 1px solid #ccc;
+                display: grid;
+                max-width: calc(550rem/16);
+                grid-template-columns: 1fr 1fr;
+                border-bottom: none;
+                border-radius: 0.5rem;
+                }
+
+                dt {
+                font-weight: 600;
+                padding: var(--spacing-sm) var(--spacing-md);
+                border-bottom: 1px solid #ccc;
+                border-right: 1px solid #ccc;
+                }
+
+                dd {
+                padding: var(--spacing-sm) var(--spacing-md);
+                border-bottom: 1px solid #ccc;
+                border-right: 1px solid #ccc;
+                font-family: monospace;
+                }`
+            }
+        </style>
+    )
+}
 
 export const Default: Story = {
-    render: () => <div>Spacing</div>
+    render: () => (
+
+        <>
+            <Style />
+            <dl>
+                {['zero', 'xs', 'sm', 'md', 'lg', 'xl'].map((key) => (
+                    <React.Fragment key={key}>
+                        <dt>{key}</dt>
+                        <dd>
+                            <SpaceCalc value={`--spacing-${key}`} />
+                        </dd>
+                    </React.Fragment>
+                ))}
+            </dl>
+        </>
+    )
 };
