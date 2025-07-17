553837f038c707c636981a146385856aca0b7a6a
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-17 10:16:29 +0200

    add goal doc and comments

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,7 @@
+// Importa il tipo 'React' dalla libreria React.
 import type React from "react";
 
+// Definisce un componente funzionale React chiamato 'Badge'.
 export const Badge: React.FC = () => {
     return <div>Badge</div>;
 }
--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -1,7 +1,11 @@
+// Importare i tipi Meta e StoryObj dalla libreria React di Storybook
 import type { Meta, StoryObj } from "@storybook/react";
 
+// Importare il componente Badge dal suo percorso definito
 import { Badge } from "../components/Badge/Badge";
 
+// Oggetto di metadati per la storia di Storybook.
+// Questo oggetto configura come il componente Badge verrà visualizzato in Storybook.
 const meta: Meta<typeof Badge> = {
     title: "Badge",
     component: Badge
@@ -9,8 +13,10 @@ const meta: Meta<typeof Badge> = {
 
 export default meta;
 
+// Definire un tipo 'Story' basato su StoryObj di Storybook,
 type Story = StoryObj<typeof meta>;
 
+// Definire la storia 'Default' per il componente Badge.
 export const Default: Story = {
     render: () => <Badge />
 };
