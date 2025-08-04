5da1f9d2be4da38222e65e992daea7b374ea94cc
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-08-04 16:55:52 +0200

    install addon-themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,8 +3,12 @@ import type { Preview } from '@storybook/react-vite'
 import "../src/styles/reset.css";
 import "../src/styles/variables.css";
 import "../src/styles/typography.css"
+// Importa il decoratore 'withThemeByClassName' per gestire i temi.
+import { withThemeByClassName } from "@storybook/addon-themes";
 
+// L'oggetto 'preview' definisce la configurazione globale di Storybook.
 const preview: Preview = {
+  // La sezione 'parameters' configura il comportamento di Storybook.
   parameters: {
     controls: {
       matchers: {
@@ -13,13 +17,26 @@ const preview: Preview = {
       },
     },
 
+    // 'a11y' configura il controllo di accessibilità (accessibility) di Storybook.
     a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
+      // 'test' definisce la modalità di reporting delle violazioni di accessibilità:
+      // 'todo' - mostra le violazioni dell'accessibilità solo nell'interfaccia di test (UI).
       test: 'todo'
-    }
+    },
   },
+  // La sezione 'decorators' permette di avvolgere le storie con logiche o componenti aggiuntivi.
+  decorators: [
+    // 'withThemeByClassName' è un decoratore che aggiunge il supporto per i temi.
+    withThemeByClassName({
+      // 'themes' mappa i nomi dei temi a classi CSS.
+      themes: {
+        light: "light-theme",
+        dark: "dark-theme",
+      },
+      defaultTheme: "light",
+      parentSelector: "body",
+    }),
+  ],
 };
 
-export default preview;
+export default preview; 
